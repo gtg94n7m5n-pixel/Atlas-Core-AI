47e02cc02784c9d0fc86c0ca68b179524f46567b
gtg94n7m5n-pixel <gtg94n7m5n@privaterelay.appleid.com>
2026-06-16 05:04:54 +0300

    Webhook sistemi kuruldu

--- bot.py
+++ bot.py
@@ -1,23 +1,18 @@
-import os
-import telebot
 from flask import Flask, request
+import os
 
-# TOKEN değişkenini Render'daki Environment Variables kısmından çekiyoruz
-TOKEN = os.environ.get('API_TOKEN')
-bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
+TOKEN = os.environ.get('API_TOKEN')
 
-# Atlas_Core_Al botu için webhook rotası
 @app.route('/' + TOKEN, methods=['POST'])
 def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
-    bot.process_new_updates([update])
-    return 'ok', 200
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return '!', 200
+    else:
+        return 'Forbidden', 403
 
-if __name__ == "__main__":
-    # Önceki webhook'u temizle ve yenisini kur
-    bot.remove_webhook()
-    bot.set_webhook(url='https://atlas-core-ai.onrender.com/' + TOKEN)
-    
-    # Sunucuyu başlat
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
