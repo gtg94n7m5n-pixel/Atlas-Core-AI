7107d383ce0de52db234286351acef273f699336
gtg94n7m5n-pixel <gtg94n7m5n@privaterelay.appleid.com>
2026-06-16 05:20:58 +0300

    webhook baglantısı ve url güncellendi

--- bot.py
+++ bot.py
@@ -1,18 +1,8 @@
-from flask import Flask, request
-import os
+# Bot nesnesini tanımla
+TOKEN = '8363347066:AAGITFGdi2C01-UECPzqG0uQyPTGxoN4_hA'
+bot = telebot.TeleBot(TOKEN)
 
-app = Flask(__name__)
-TOKEN = os.environ.get('API_TOKEN')
-
-@app.route('/' + TOKEN, methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return '!', 200
-    else:
-        return 'Forbidden', 403
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+# Webhook'u buraya zorla (linkini düzelttik)
+webhook_url = 'https://atlas-core-ai.onrender.com/' + TOKEN
+bot.remove_webhook()
+bot.set_webhook(url=webhook_url)
