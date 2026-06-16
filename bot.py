80cc7d4203b7486e8a2c6dabd1fcba8c1293fe97
gtg94n7m5n-pixel <gtg94n7m5n@privaterelay.appleid.com>
2026-06-16 14:21:04 +0300

    Web servisi işleme mantığı Güncellendi

--- bot.py
+++ bot.py
@@ -6,18 +6,20 @@ TOKEN = '8363347066:AAGITFGdi2C01-UECPzqG0uQyPTGxoN4_hA'
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
+# Mesajları karşılayan kısım
 @app.route('/' + TOKEN, methods=['POST'])
 def webhook():
-    json_str = request.get_data().decode('UTF-8')
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return 'OK', 200
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return 'OK', 200
+    return 'Forbidden', 403
 
+# Render'ın sürekli kontrol ettiği "sağlık" rotası
 @app.route('/')
 def index():
-    return 'Bot is running!'
+    return 'Bot aktif durumda.', 200
 
 if __name__ == '__main__':
-    bot.remove_webhook()
-    bot.set_webhook(url='https://atlas-core-ai.onrender.com/' + TOKEN)
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
