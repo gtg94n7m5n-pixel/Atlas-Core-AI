5abebea5d5adc89f875ce1af535855fbf9cda153
gtg94n7m5n-pixel <gtg94n7m5n@privaterelay.appleid.com>
2026-06-16 13:56:07 +0300

    Web servisi ve port tanımları Düzeltıldı

--- bot.py
+++ bot.py
@@ -1,13 +1,23 @@
 import telebot
+from flask import Flask, request
 import os
 
 TOKEN = '8363347066:AAGITFGdi2C01-UECPzqG0uQyPTGxoN4_hA'
 bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Bot aktif, mesajın ulaştı!")
+@app.route('/' + TOKEN, methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return 'OK', 200
 
-# Webhook'u devre dışı bırakıp, botun doğrudan Telegram'a bağlanmasını sağla
-bot.remove_webhook()
-bot.polling(none_stop=True)
+@app.route('/')
+def index():
+    return 'Bot is running!'
+
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url='https://atlas-core-ai.onrender.com/' + TOKEN)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
