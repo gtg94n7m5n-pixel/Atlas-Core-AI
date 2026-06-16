c5ebb3df72384aba23af415eb1c03c483d9c55eb
gtg94n7m5n-pixel <gtg94n7m5n@privaterelay.appleid.com>
2026-06-16 13:45:22 +0300

    polling yonetımıne dönüldü

--- bot.py
+++ bot.py
@@ -1,21 +1,13 @@
 import telebot
-from flask import Flask, request
 import os
 
 TOKEN = '8363347066:AAGITFGdi2C01-UECPzqG0uQyPTGxoN4_hA'
 bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 
-@app.route('/' + TOKEN, methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return '!', 200
-    return 'Forbidden', 403
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Bot aktif, mesajın ulaştı!")
 
-if __name__ == '__main__':
-    bot.remove_webhook()
-    bot.set_webhook(url='https://atlas-core-ai.onrender.com/' + TOKEN)
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+# Webhook'u devre dışı bırakıp, botun doğrudan Telegram'a bağlanmasını sağla
+bot.remove_webhook()
+bot.polling(none_stop=True)
